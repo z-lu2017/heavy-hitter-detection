3c6f9a163ea08f37e64e9b2fcf4bc99b8382582c
Masoud Moshref <masoud.moshref@barefootnetworks.com>
2017-07-21 18:21:37 -0700

    mri works now completely

--- SIGCOMM_2017/exercises/mri/send.py
+++ SIGCOMM_2017/exercises/mri/send.py
@@ -9,8 +9,10 @@ import struct
 from scapy.all import sendp, send, hexdump, get_if_list, get_if_hwaddr
 from scapy.all import Packet, IPOption
 from scapy.all import Ether, IP, UDP
-from scapy.all import IntField, FieldListField, FieldLenField, ShortField
+from scapy.all import IntField, FieldListField, FieldLenField, ShortField, PacketListField
 from scapy.layers.inet import _IPOption_HDR
+
+from time import sleep
 
 def get_if():
     ifs=get_if_list()
@@ -24,18 +26,24 @@ def get_if():
         exit(1)
     return iface
 
+class SwitchTrace(Packet):
+    fields_desc = [ IntField("swid", 0),
+                  IntField("qdepth", 0)]
+    def extract_padding(self, p):
+                return "", p
+
 class IPOption_MRI(IPOption):
     name = "MRI"
     option = 31
     fields_desc = [ _IPOption_HDR,
                     FieldLenField("length", None, fmt="B",
-                                  length_of="swids",
-                                  adjust=lambda pkt,l:l+4),
+                                  length_of="swtraces",
+                                  adjust=lambda pkt,l:l*2+4),
                     ShortField("count", 0),
-                    FieldListField("swids",
+                    PacketListField("swtraces",
                                    [],
-                                   IntField("", 0),
-                                   length_from=lambda pkt:pkt.count*4) ]
+                                   SwitchTrace,
+                                   count_from=lambda pkt:(pkt.count*1)) ]
 
 
 def main():
@@ -47,11 +55,23 @@ def main():
     addr = socket.gethostbyname(sys.argv[1])
     iface = get_if()
 
-#    pkt = Ether(src=get_if_hwaddr(iface), dst="ff:ff:ff:ff:ff:ff") / IP(dst=addr) / UDP(dport=4321, sport=1234) / sys.argv[2]
-    pkt = Ether(src=get_if_hwaddr(iface), dst="ff:ff:ff:ff:ff:ff") / IP(dst=addr, options = IPOption_MRI(count=2, swids=[3,4])) / UDP(dport=4321, sport=1234) / sys.argv[2]
+    pkt = Ether(src=get_if_hwaddr(iface), dst="ff:ff:ff:ff:ff:ff") / IP(
+        dst=addr, options = IPOption_MRI(count=0,
+            swtraces=[])) / UDP(
+            dport=4321, sport=1234) / sys.argv[2]
+
+ #   pkt = Ether(src=get_if_hwaddr(iface), dst="ff:ff:ff:ff:ff:ff") / IP(
+ #       dst=addr, options = IPOption_MRI(count=2,
+ #           swtraces=[SwitchTrace(swid=0,qdepth=0), SwitchTrace(swid=1,qdepth=0)])) / UDP(
+ #           dport=4321, sport=1234) / sys.argv[2]
     pkt.show2()
     #hexdump(pkt)
-    sendp(pkt, iface=iface)
+    try:
+      for i in range(int(sys.argv[3])):
+        sendp(pkt, iface=iface)
+        sleep(1)
+    except KeyboardInterrupt:
+        raise
 
 
 if __name__ == '__main__':
