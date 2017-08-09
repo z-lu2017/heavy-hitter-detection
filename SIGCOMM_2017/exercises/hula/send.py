dfab41fcb4f56827538f0ef763062c478293da27
Masoud Moshref <masoud.moshref@barefootnetworks.com>
2017-08-09 14:06:13 -0700

    fixes digest. keep perflow nhop. first correct result

--- SIGCOMM_2017/exercises/hula/send.py
+++ SIGCOMM_2017/exercises/hula/send.py
@@ -10,13 +10,22 @@ from scapy.all import Packet
 from scapy.all import Ether, IP, UDP
 from scapy.fields import *
 from time import sleep
-
-period = 10
+import crcmod
 
 class Hula(Packet):
    fields_desc = [ BitField("dir", 0, 1),
                    BitField("qdepth", 0, 15),
-                   BitField("digest", 0, 32),]
+                   XIntField("digest", None)]
+   def post_build(self, p, pay):
+        p += pay
+        if self.digest is None:
+            crc32 = crcmod.Crc(0x104c11db7, initCrc=0, xorOut=0xFFFFFFFF)
+            crc32.update(str(p));
+            c = bytes(bytearray.fromhex("%08x" % crc32.crcValue))
+            p = p[:2]+ c +p[6:]
+            #ck = checksum(p)
+            #p = p[:2]+"\x00\x00"+chr(ck>>8)+chr(ck&0xff)+p[6:]
+        return p
 
 class SourceRoute(Packet):
    fields_desc = [ BitField("bos", 0, 1),
@@ -46,19 +55,17 @@ def main():
 
 
     try:
-        while(True):
-          for e in info:
-            ports = e[2]
-            pkt =  Ether(src=get_if_hwaddr(e[3]), dst='ff:ff:ff:ff:ff:ff')
-            pkt = pkt / Hula(dir=0, qdepth=0, digest=0)
-            pkt = pkt / SourceRoute(bos=0, port=ports[0])
-            pkt = pkt / SourceRoute(bos=0, port=ports[1])
-            pkt = pkt / SourceRoute(bos=0, port=ports[2])
-            pkt = pkt / SourceRoute(bos=1, port=ports[3])
-            pkt = pkt / IP(dst=e[1], src=e[0]) / UDP(dport=4321, sport=1234)
-            pkt.show2()
-            sendp(pkt, iface=e[3], verbose=False)
-          sleep(period)
+      for e in info:
+        ports = e[2]
+        pkt =  Ether(src=get_if_hwaddr(e[3]), dst='ff:ff:ff:ff:ff:ff')
+        pkt = pkt / Hula(dir=0, qdepth=0)
+        pkt = pkt / SourceRoute(bos=0, port=ports[0])
+        pkt = pkt / SourceRoute(bos=0, port=ports[1])
+        pkt = pkt / SourceRoute(bos=0, port=ports[2])
+        pkt = pkt / SourceRoute(bos=1, port=ports[3])
+        pkt = pkt / IP(dst=e[1], src=e[0]) / UDP(dport=4321, sport=1234)
+        pkt.show2()
+        sendp(pkt, iface=e[3], verbose=False)
     except KeyboardInterrupt:
         raise
 
