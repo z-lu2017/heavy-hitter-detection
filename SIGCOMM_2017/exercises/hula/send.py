9c4ed00acc784a1b294d761e493019b405c08f91
Masoud Moshref <masoud.moshref@barefootnetworks.com>
2017-08-09 14:46:58 -0700

    add comments

--- SIGCOMM_2017/exercises/hula/send.py
+++ SIGCOMM_2017/exercises/hula/send.py
@@ -37,6 +37,9 @@ bind_layers(SourceRoute, SourceRoute, bos=0)
 bind_layers(SourceRoute, IP, bos=1)
 
 def main():
+    period = 0
+    if len(sys.argv) > 1:
+        period = int(sys.argv[1])
 
             # src,     dst     , src routing , interface
     info = [
@@ -55,17 +58,22 @@ def main():
 
 
     try:
-      for e in info:
-        ports = e[2]
-        pkt =  Ether(src=get_if_hwaddr(e[3]), dst='ff:ff:ff:ff:ff:ff')
-        pkt = pkt / Hula(dir=0, qdepth=0)
-        pkt = pkt / SourceRoute(bos=0, port=ports[0])
-        pkt = pkt / SourceRoute(bos=0, port=ports[1])
-        pkt = pkt / SourceRoute(bos=0, port=ports[2])
-        pkt = pkt / SourceRoute(bos=1, port=ports[3])
-        pkt = pkt / IP(dst=e[1], src=e[0]) / UDP(dport=4321, sport=1234)
-        pkt.show2()
-        sendp(pkt, iface=e[3], verbose=False)
+      while True:
+          for e in info:
+            ports = e[2]
+            pkt =  Ether(src=get_if_hwaddr(e[3]), dst='ff:ff:ff:ff:ff:ff')
+            pkt = pkt / Hula(dir=0, qdepth=0)
+            pkt = pkt / SourceRoute(bos=0, port=ports[0])
+            pkt = pkt / SourceRoute(bos=0, port=ports[1])
+            pkt = pkt / SourceRoute(bos=0, port=ports[2])
+            pkt = pkt / SourceRoute(bos=1, port=ports[3])
+            pkt = pkt / IP(dst=e[1], src=e[0]) / UDP(dport=4321, sport=1234)
+            #pkt.show2()
+            sendp(pkt, iface=e[3], verbose=False)
+          if period == 0:
+              break;
+          else:
+              sleep(period)
     except KeyboardInterrupt:
         raise
 
