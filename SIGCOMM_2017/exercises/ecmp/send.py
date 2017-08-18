a4639dc70eee9114af01846c0050911cfb14a983
Nate Foster <jnfoster@cs.cornell.edu>
2017-08-18 00:25:09 -0400

    Checkpoint ECMP

--- SIGCOMM_2017/exercises/ecmp/send.py
+++ SIGCOMM_2017/exercises/ecmp/send.py
@@ -31,7 +31,7 @@ def main():
     iface = get_if()
 
     print "sending on interface %s to %s" % (iface, str(addr))
-    pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff') / IP(dst=addr) / UDP(dport=4321, sport=1236) / sys.argv[2]
+    pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff') / IP(dst=addr) / UDP(dport=4321, sport=random.randint(49152,65535)) / sys.argv[2]
     pkt.show2()
     sendp(pkt, iface=iface, verbose=False)
 
