1c8c14b3d91e793cae3651a7e0b574dd3de567cd
Masoud Moshref <masoud.moshref@barefootnetworks.com>
2017-08-18 07:03:19 -0700

    fix sniff filter

--- SIGCOMM_2017/exercises/ecmp/receive.py
+++ SIGCOMM_2017/exercises/ecmp/receive.py
@@ -45,7 +45,7 @@ def main():
     iface = ifaces[0]
     print "sniffing on %s" % iface
     sys.stdout.flush()
-    sniff(filter="udp and port 4321", iface = iface,
+    sniff(filter="tcp", iface = iface,
           prn = lambda x: handle_pkt(x))
 
 if __name__ == '__main__':
