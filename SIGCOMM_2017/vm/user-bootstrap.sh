adcd8dfe05ce6d9e526d29e3c58adaf015f5a8a0
Nate Foster <jnfoster@cs.cornell.edu>
2017-08-19 07:24:16 -0400

    Small fix

--- SIGCOMM_2017/vm/user-bootstrap.sh
+++ SIGCOMM_2017/vm/user-bootstrap.sh
@@ -27,11 +27,13 @@ cd ..
 # P4C
 git clone --recursive https://github.com/p4lang/p4c
 sudo apt-get install g++ git automake libtool libgc-dev bison flex libfl-dev libgmp-dev libboost-dev libboost-iostreams-dev pkg-config python python-scapy python-ipaddr tcpdump cmake
+cd p4c
 mkdir build
 cd build
 cmake ..
 make -j4
 sudo make install
+cd ..
 cd ..
 
 # Tutorials
