21dfe2be9bc79ffe10774fd36478f03a3ef4b5e9
Nate Foster <jnfoster@cs.cornell.edu>
2017-08-24 01:20:57 -0400

    Updated build scripts for Vagrant VM

--- SIGCOMM_2017/vm/user-bootstrap.sh
+++ SIGCOMM_2017/vm/user-bootstrap.sh
@@ -35,9 +35,16 @@ cd ..
 cd ..
 
 # Tutorials
+pip install crcmod
 git clone https://github.com/p4lang/tutorials
 cd tutorials
 git checkout sigcomm_17
 cd ..
 sudo mv tutorials /home/p4
 sudo chown -R p4:p4 /home/p4/tutorials
+
+# Emacs
+sudo cp p4_16-mode.el /usr/share/emacs/site-lisp/
+sudo echo "(add-to-list 'auto-mode-alist '(\"\\.p4\\'\" . p4_16-mode))" >> /home/p4/.emacs
+sudo chown p4:p4 /home/p4/.emacs
+
